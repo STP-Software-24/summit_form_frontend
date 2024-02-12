91fe3f43cc6f6f06ffa9c91311c9b1fcacfa3f98
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-02-12 23:34:10 +0200

    add img

--- src/component/RegistrationForm.jsx
+++ src/component/RegistrationForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import {useNavigate} from "react-router-dom"
 import FormInput from "./FormInput";
+import "./RegistrationForm.css"
 import axios from "axios";
 
 const RegistrationForm = () => {
