9ac1c9924202174f36d2c4b29c6cff4f912381a4
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-02-13 10:17:08 +0200

    button borders

--- src/component/FormInput.jsx
+++ src/component/FormInput.jsx
@@ -3,29 +3,31 @@
 /* eslint-disable react/no-unknown-property */
 import { useState } from "react";
 import "./FormInput.css";
-const FormInput =(props)=> {
-  const[focused,setFocused]=useState(false);
-const {label,errorMessage ,onChange, id, ...inputProps}= props;
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, validation, value, ...inputProps } = props;
 
-const handleFocus=(e)=>{
-  setFocused(false);
-};
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
 
-  return(
-    <div className="p-2 border-rounded-2 ">
-     <label >{label}</label  > 
-      <input 
-      {...inputProps} 
-      onChange={onChange} 
-      onBlur={handleFocus} 
-      onFocus={()=>
-        inputProps.name===""||setFocused(true)}
-        focused ={focused.toString()}
-        className="form-control  "/> 
-      <span>{errorMessage}</span>
-   
-    </div> 
+  const handleBlur = (e) => {
+    setFocused(false);
+  }
 
+  return (
+    <div className="p-2 border-rounded-2">
+      <label className="fs-4 fw-bold">{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() => inputProps.name === "" || setFocused(true)}
+        focused={focused.toString()}
+        className={`form-control  black-border ${focused && validation(value) ? 'invalid': ''}`}
+      />
+      {focused && validation(value) && <span>{errorMessage}</span>}
+    </div>
   );
 };
 export default FormInput;
